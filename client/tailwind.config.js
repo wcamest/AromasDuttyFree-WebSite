ef1eee190d791c17d08362b9e62ea5e77b4b81b9
wcamest <willkmiloeo1228@gmail.com>
2023-06-07 15:47:15 -0500

    feat.: Layout y estructira de la pagina de inicio

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      scale: {
+        '200': '2',
+        '300': '3',
+      }
     },
   },
   plugins: [],
